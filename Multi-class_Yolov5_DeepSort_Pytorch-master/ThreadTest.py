4839214e6af1798e259e3e06958674c90a8e7530
Jonas Lindberg <jonas.lindberg@sigmatechnology.se>
2021-02-08 17:23:27 +0100

    New video, changed threadtest

--- Multi-class_Yolov5_DeepSort_Pytorch-master/ThreadTest.py
+++ Multi-class_Yolov5_DeepSort_Pytorch-master/ThreadTest.py
@@ -13,10 +13,18 @@ def trackerCameraWest(path):
 def trackerCameraSouthWest(path):
     q2.put(runTracker(path))
 
+def trackerCameraMiddleFacingWestSouth(path):
+    q3.put(runTracker(path))
+
 
 def consumer():
-    item = q.get()
-    item2 = q2.get()
+    itemCamWest = q.get()
+    itemCamSouthWest = q2.get()
+    itemCamMiddleFacingWestSouth = q3.get()
+
+    for outputWest, outputSouthWest, outputMiddleWestSouth in zip(itemCamWest, itemCamSouthWest, itemCamMiddleFacingWestSouth):
+
+
     for x, x2 in zip(item,item2):
         for i,i2 in zip(x,x2):
             print(i,i2)
@@ -25,8 +33,14 @@ if __name__ == '__main__':
 
     q = queue.Queue()
     q2 = queue.Queue()
-    thread1 = threading.Thread(target=trackerCameraWest, args=('vid4.mkv',)).start()
-    thread2 = threading.Thread(target=trackerCameraSouthWest, args=('vid6.mkv',)).start()
+    q3 = queue.Queue()
+
+    # Produce
+    threadCameraWest = threading.Thread(target=trackerCameraWest, args=('CameraWest.mkv',)).start()
+    threadCameraSouthWest = threading.Thread(target=trackerCameraSouthWest, args=('CameraSouthWest.mkv',)).start()
+    threadCameraMiddleFacingWestSouth = threading.Thread(target=trackerCameraSouthWest, args=('CameraMiddleFacingWestSouth.mkv',)).start()
+
+    # Consumer
     consumerThread = threading.Thread(target=consumer).start()
 
 
