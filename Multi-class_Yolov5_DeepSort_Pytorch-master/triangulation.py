568ee52239b8e54d34859b9a6b7bf7b65d0eca18
Jonas Lindberg <jonas.lindberg@sigmatechnology.se>
2021-02-09 14:27:34 +0100

    Checkar ut

--- Multi-class_Yolov5_DeepSort_Pytorch-master/triangulation.py
+++ Multi-class_Yolov5_DeepSort_Pytorch-master/triangulation.py
@@ -1,10 +1,12 @@
-def testFunc(array):
-    for item in array:
-        print('Här e nått: ', item)
+from shapely.geometry import LineString
 
 class Triangulator:
     def __init__(self, cameraPos):
         self.camPos = cameraPos
 
-    def triangulate(self, projPoints1, projPoints2):
-        if
+    def triangulate(self, projPoints1,cameraPos1, projPoints2, cameraPos2):
+        triangulatedPoints = []
+        for p1 in projPoints1:
+            for p2 in projPoints2:
+                line1 = LineString([(cameraPos1), (p1)])
+
