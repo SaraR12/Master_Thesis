722d0d107c32043bfae95e59b0031fb2df520191
Jonas Lindberg <jonas.lindberg@sigmatechnology.se>
2021-02-16 17:00:50 +0100

    Jobbat hela tisdagen =)

--- Multi-class_Yolov5_DeepSort_Pytorch-master/Mapping/bbox_intersection.py
+++ Multi-class_Yolov5_DeepSort_Pytorch-master/Mapping/bbox_intersection.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 from deep_sort.deep_sort.sort.iou_matching import *
 from Mapping.mapper import *
-
-def draw_multiple_boxes(bbox_list, mapping_objects, identities_list, offset=(0,0)):
+CLASSES = ['AGV', 'Human']
+def draw_multiple_boxes(bbox_list, mapping_objects, identities_list, classes_list, offset=(0,0)):
     # bbox_list = list with bounding boxes from cameras
     # mapping_objects = Homography from the different cameras
     mappedImg = cv2.imread('Mapping/plane.png') #PLANAR_MAP
-    for bbox, mapperObject, identities in zip(bbox_list, mapping_objects, identities_list):
+    for bbox, mapperObject, identities, cls in zip(bbox_list, mapping_objects, identities_list, classes_list):
         for i, box in enumerate(bbox):
             x1, y1, x2, y2 = [int(i) for i in box]
             x1 += offset[0]
@@ -23,18 +23,19 @@ def draw_multiple_boxes(bbox_list, mapping_objects, identities_list, offset=(0,0
 
             # Mapping to plane
             x1m, x2m, y1m, y2m, color = mapperObject.mapBoundingBoxPoints(x1, x2, y1, y2, color)
-            cv2.rectangle(mappedImg, (x1m, y1m), (x2m, y2m), color, 2)
-            #cv2.line(mappedImg, (x1m, y1m), (x1m, y2m), color, 2)  # left line
-            #cv2.line(mappedImg, (x2m, y1m), (x2m, y2m), color, 2)  # right line
-            #cv2.line(mappedImg, (x1m, y1m), (x2m, y1m), color, 2)  # top line
-            #cv2.line(mappedImg, (x1m, y2m), (x2m, y2m), color, 2)  # bottom line
-            # cv2.rectangle(mappedImg, (x1m, y1m), (x2m, y2m), color, 1)
-            # allMappedPoints.append(mappedPoint)
 
+            cv2.rectangle(mappedImg, (x1m, y1m), (x2m, y2m), color, 2)
+
+            label = CLASSES[cls]
+
+
+            t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
+            cv2.rectangle(mappedImg, (x1m, y1m), (x1m + t_size[0] + 3, y1m + t_size[1] + 4), color, -1)
+            cv2.putText(mappedImg, label, (x1m, y1m + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
     return mappedImg
 
-def draw_bboxes(bboxlist):
-    mappedImg = cv2.imread('Mapping/plane.png')
+def draw_bboxes(bboxlist, mappedImg):
+    #mappedImg = cv2.imread('Mapping/plane.png')
     for bbox in bboxlist:
         #print(bbox)
         x1 = bbox[0]
@@ -45,7 +46,7 @@ def draw_bboxes(bboxlist):
         cv2.rectangle(mappedImg, (x1, y1), (x2, y2), color, 2)
     return mappedImg
 
-def iou_bboxes(bbox_list, mapObjects_list):
+def iou_bboxes(bbox_list, mapObjects_list, cam_id_list, classes_list):
     bbox_all_list = np.array([[1,2,3,4]], dtype=int)
     # Collect all bboxes
     j = 0
@@ -84,10 +85,11 @@ def iou_bboxes(bbox_list, mapObjects_list):
     deleted_bbox = []
 
     for bbox in bbox_all_list:
+        print('bbox_all_list', bbox_all_list)
+        print('cam_id_list',cam_id_list)
+        bbox_cam_ID = cam_id_list.pop(0)
+        bbox_class = classes_list.pop(0)
 
-        #print('Active bbox: ', bbox)
-        #print('deleted_bbox', deleted_bbox)
-       # if [bbox] not in deleted_bbox:
         GO = True
         for box in deleted_bbox:
             if all(bbox == box):
@@ -96,22 +98,17 @@ def iou_bboxes(bbox_list, mapObjects_list):
         if GO:
             if candidates.shape[0] > 1:
                 candidates = np.delete(candidates, 0, axis=0)
-                #print('candidates', candidates)
-                intersected_bbox, matched_bbox_index = intersection(bbox, candidates)
-                #print('matched_bbox_index', matched_bbox_index)
+                intersected_bbox, matched_bbox_index = intersection(bbox, bbox_cam_ID, bbox_class, candidates,
+                                                                    cam_id_list, classes_list)
 
                 if matched_bbox_index is not None:
-                    #print('Matched bbox: ', candidates[matched_bbox_index, :])
                     deleted_bbox.append(candidates[matched_bbox_index, :].tolist())
                     candidates = np.delete(candidates, matched_bbox_index, axis=0)
 
                 matches = np.append(matches, [intersected_bbox], axis=0)
 
-                #print('matched bbox = ', intersected_bbox)
             else: # If no bounding boxes match. Save the current one
                 intersected_bbox = [bbox[0], bbox[1], bbox[0]+bbox[3], bbox[1]+bbox[2]]
-                #print('NONE ----->: ', intersected_bbox)
-
 
                 matches = np.append(matches, [intersected_bbox], axis=0)
 
@@ -119,6 +116,60 @@ def iou_bboxes(bbox_list, mapObjects_list):
 
     return matches
 
+def intersection(bbox, bbox_cam_ID, bbox_class, candidates, cam_id_list, classes_list):
+    bb1x1 = bbox[0]
+    bb1y1 = bbox[1]
+    bb1x2 = bbox[0] + bbox[3]
+    bb1y2 = bbox[1] + bbox[2]
+    area_of_bbox = bbox[2] * bbox[3]
+
+    # determine the coordinates of the intersection rectangle
+    iouList = []
+    intersection_bbox = []
+    for (i, candidate), Cam_ID, cls in zip(enumerate(candidates), cam_id_list, classes_list):
+        if (Cam_ID is not bbox_cam_ID) and (cls == bbox_class):
+            bb2x1 = candidate[0]
+            bb2y1 = candidate[1]
+            bb2x2 = candidate[0] + candidate[3]
+            bb2y2 = candidate[1] + candidate[2]
+
+            x_left = max(bb1x1, bb2x1)
+            y_top = max(bb1y1, bb2y1)
+            x_right = min(bb1x2, bb2x2)
+            y_bottom = min(bb1y2, bb2y2)
+
+
+            # The intersection of two axis-aligned bounding boxes is always an
+            # axis-aligned bounding box
+            intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+            # compute the area of both AABBs
+            bb1_area = (bb1x2 - bb1x1) * (bb1y2 - bb1y1)
+            bb2_area = (bb2x2 - bb2x1) * (bb2y2 - bb2y1)
+
+            # compute the intersection over union by taking the intersection
+            # area and dividing it by the sum of prediction + ground-truth
+            # areas - the interesection area
+            if x_right < x_left or y_bottom < y_top:
+                iouList.append(0.0)
+            else:
+                iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+                iouList.append(iou)
+                if any(iou > item for item in iouList):
+                    bbox_h = y_bottom - y_top
+                    bbox_w = x_right - x_left
+                    intersection_bbox = [x_left, y_top, x_right, y_bottom]
+                    index = i
+
+
+    #if len(iouList > 0) > 1:
+
+
+    if intersection_bbox == []:
+        intersection_bbox = [bb1x1, bb1y1, bb1x2, bb1y2]
+        index = None
+
+    return intersection_bbox, index
 
 
 def compute_color_for_labels(label):
@@ -130,55 +181,6 @@ def compute_color_for_labels(label):
     color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
     return tuple(color)
 
-def intersection(bbox, candidates):
-    bb1x1 = bbox[0]
-    bb1y1 = bbox[1]
-    bb1x2 = bbox[0] + bbox[3]
-    bb1y2 = bbox[1] + bbox[2]
-    area_of_bbox = bbox[2] * bbox[3]
-
-    # determine the coordinates of the intersection rectangle
-    iouList = []
-    intersection_bbox = []
-    for i, candidate in enumerate(candidates):
-        bb2x1 = candidate[0]
-        bb2y1 = candidate[1]
-        bb2x2 = candidate[0] + candidate[3]
-        bb2y2 = candidate[1] + candidate[2]
-
-        x_left = max(bb1x1, bb2x1)
-        y_top = max(bb1y1, bb2y1)
-        x_right = min(bb1x2, bb2x2)
-        y_bottom = min(bb1y2, bb2y2)
-
-
-        # The intersection of two axis-aligned bounding boxes is always an
-        # axis-aligned bounding box
-        intersection_area = (x_right - x_left) * (y_bottom - y_top)
-
-        # compute the area of both AABBs
-        bb1_area = (bb1x2 - bb1x1) * (bb1y2 - bb1y1)
-        bb2_area = (bb2x2 - bb2x1) * (bb2y2 - bb2y1)
-
-        # compute the intersection over union by taking the intersection
-        # area and dividing it by the sum of prediction + ground-truth
-        # areas - the interesection area
-        if x_right < x_left or y_bottom < y_top:
-            iouList.append(0.0)
-        else:
-            iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
-            iouList.append(iou)
-            if any(iou > item for item in iouList):
-                bbox_h = y_bottom - y_top
-                bbox_w = x_right - x_left
-                intersection_bbox = [x_left, y_top, x_right, y_bottom]
-                index = i
-
-    if intersection_bbox == []:
-        intersection_bbox = [bb1x1, bb1y1, bb1x2, bb1y2]
-        index = None
-
-    return intersection_bbox, index
 """list = [[1,2],[3,4]]
 print(list.pop(0))
 print(list)"""
@@ -186,5 +188,3 @@ print(list)"""
 q1 = [[None],[2,1]] #,[None], [np.array([[367, 443, 480, 549,   1,   0,  92,   3],[764, 525, 806, 586,   3,   1,  86,   3],[401, 349, 517, 424,   5,   0,  80,   3]])],
       #[np.array([[367, 442, 480, 550,   1,   0,  92,   4], [762, 522, 807, 589,   3,   1,  85,   4],[403, 349, 518, 423,   5,   0,  81,   4]])], ]
 
-
-print(all(q1))
--- Multi-class_Yolov5_DeepSort_Pytorch-master/track.py
+++ Multi-class_Yolov5_DeepSort_Pytorch-master/track.py
@@ -297,11 +297,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='yolov5/weights/yolov5s.pt', help='model.pt path')
     parser.add_argument('--data', type=str, default='yolov5/data/data.yaml', help='data yaml path') # Class names
-    parser.add_argument('--source', type=str, default='videos/videoMSW.mkv', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='videos/videoMW.mkv', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.61, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--iou-thres', type=float, default=0.1, help='IOU threshold for NMS')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
@@ -323,9 +323,10 @@ if __name__ == '__main__':
 
     test = []
     with torch.no_grad():
-        out = detect(args, device, 'MSW')
+        out = detect(args, device, 'MW')
         for i in out:
             test.append(i)
+
 def run(path, camera, queue = None):
     print(path)
     parser = argparse.ArgumentParser()
